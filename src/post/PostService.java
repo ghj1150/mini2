4fe5795a3761cdf4cd167cfa0571ca12a950f8d3
ghj1150 <lgc1150@naver.com>
2024-09-29 22:23:31 +0900

    수정 및 삭제 기능 작업중

--- src/post/PostService.java
+++ src/post/PostService.java
@@ -6,16 +6,18 @@ import mini.miniUtils;
 
 public class PostService {
 	private List<Post> posts = new ArrayList<Post>();
-	public int user_id = 2;
+	public int userId = 2;
 
 	{
 		posts = miniUtils.dataLoad("./src/data/post.ser");
+
 		if (posts == null) {
 			posts = new ArrayList<Post>();
 			posts.add(new Post(1, 1, "제목", "히히히", "09/26"));
 			posts.add(new Post(2, 2, "제목", "호호호", "09/26"));
 			posts.add(new Post(3, 3, "제목", "허허허", "09/26"));
 			posts.add(new Post(4, 4, "제목", "후후후", "09/26"));
+
 			System.out.println("기본 데이터 추가");
 		}
 //		System.out.println(posts);
@@ -28,7 +30,8 @@ public class PostService {
 
 		while (true) {
 
-			int input = miniUtils.next("1.목록보기 2. 글쓰기", Integer.class, n -> n > 0 && n <= 2, "1~2사이의 값을 입력하세요");
+			int input = miniUtils.next("1.목록보기 2. 글쓰기 3. 종료", Integer.class, n -> n > 0 && n <= 3, "1~3사이의 값을 입력하세요");
+
 			switch (input) {
 			case 1:
 				list();
@@ -36,16 +39,20 @@ public class PostService {
 			case 2:
 				add();
 				break;
+			case 3:
+				System.out.println("종료");
+				return;
 			default:
 				break;
+
 			}
 		}
-
 	}
 
 	// 글쓰기
 	public void add() {
 //		System.out.println(posts);
+
 		int idx = miniUtils.next("id", Integer.class, n -> n > 0, "");
 		int user_id = miniUtils.next("회원 id", Integer.class, n -> n > 0, "");
 		String title = miniUtils.next("제목", String.class);
@@ -56,6 +63,7 @@ public class PostService {
 	}
 
 	// 목록보기
+
 	public void list() {
 
 		System.out.println("                게시판 목록                   ");
@@ -64,20 +72,22 @@ public class PostService {
 			System.out.println(posts.get(i));
 		}
 
-		int t = miniUtils.next("몇번?", Integer.class);
+//		int t = miniUtils.next("몇번?", Integer.class);
+//
+//		Post post = null;
+//		for (int i = 0; i < posts.size(); i++) {
+//			if (posts.get(i).getIdx() == t) {
+//				post = posts.get(i);
+//			}
+//		}
+//		if (post == null) {
+//			System.out.println("게시글이 없습니다");
+//			return;
+//		}
+//
+		Post p = findByIdx(miniUtils.next("몇번?", Integer.class, n -> findByIdx(n) != null, "게시글이 없습니다"));
 
-		Post post = null;
-		for (int i = 0; i < posts.size(); i++) {
-			if (posts.get(i).getIdx() == t) {
-				post = posts.get(i);
-			}
-		}
-		if (post == null) {
-			System.out.println("게시글이 없습니다");
-			return;
-		}
-
-		readpost(post);
+		readpost(p);
 	}
 
 	// 게시글 내용
@@ -91,19 +101,21 @@ public class PostService {
 
 		System.out.println("                               날짜 : " + post.getCreateDate());
 		System.out.println("---------------------------------------------");
+
 		// 종료 버튼
 		// 수정 삭제는 post를 작성한 user만 수정 삭제 가능하게
+
 		while (true) {
 			int input = miniUtils.next("1.수정하기 2. 삭제하기 3. 종료", Integer.class, n -> n > 0 && n <= 3, "1~3사이의 값을 입력하세요");
+
 			switch (input) {
 			case 1:
-				modify();
-				break;
+				modify(post);
+				return;
 			case 2:
-				remove();
-				break;
+				remove(post);
+				return;
 			case 3:
-				System.out.println("종료");
 				return;
 			default:
 				break;
@@ -112,23 +124,43 @@ public class PostService {
 	}
 
 	// 수정
-	public void modify() {
+	public void modify(Post p) {
+
+//		Post p = findById(miniUtils.next("몇번?", Integer.class, n -> findById(n) != userId, null));
+
 		String title = miniUtils.next("제목", String.class);
 		String post = miniUtils.next("게시글", String.class);
+		p.setTitle(title);
+		p.setPost(post);
+
+		System.out.println("수정완료");
 	}
 
 	// 삭제
-	public void remove() {
-
+	public void remove(Post p) {
+//		Post p = findById(miniUtils.next("삭제할 글", Integer.class, null, null));
+		posts.remove(p);
+		System.out.println("삭제완료");
 	}
-	
-//	private Post findBy(int user_id) {
+
+//	private Post findById(int userId) {
 //		Post post = null;
-//		for(int i = 0; i < posts.size(); i++) {
-//			if(posts.get(i).getUserId() == user_id) {
+//		for (int i = 0; i < posts.size(); i++) {
+//			if (posts.get(i).getUserId() == userId) {
 //				post = posts.get(i);
 //			}
 //		}
+
 //		return post;
 //	}
+
+	private Post findByIdx(int idx) {
+		Post post = null;
+		for (int i = 0; i < posts.size(); i++) {
+			if (posts.get(i).getIdx() == idx) {
+				post = posts.get(i);
+			}
+		}
+		return post;
+	}
 }
