3f8a633fa3c76d0ac7df3ac5bf6c78bc8be94926
ghj1150 <lgc1150@naver.com>
2024-10-03 19:48:00 +0900

    분석 승환 요청

--- src/accountbook/Analyze.java
+++ src/accountbook/Analyze.java
@@ -1,11 +1,64 @@
 package accountbook;
 
 public class Analyze {
-    private String userId;
-    private int totalIncome; // 총수입 (이월)
-    private int totalLosses; // 총지출 (이번달)
-    private int remainMoney; // 남은 잔액 (총수입 - 지출)
-    private int expenseIncomeRate; // 수입대비 지출 퍼센트
+	private String userId;
+	private int totalIncome; // 총수입 (이월)
+	private int totalLosses; // 총지출 (이번달)
+	private int remainMoney; // 남은 잔액 (총수입 - 지출)
+	private int expenseIncomeRate; // 수입대비 지출 퍼센트
 
-    
+	public Analyze(String userId, int totalIncome, int totalLosses, int remainMoney, int expenseIncomeRate) {
+		this.userId = userId;
+		this.totalIncome = totalIncome;
+		this.totalLosses = totalLosses;
+		this.remainMoney = remainMoney;
+		this.expenseIncomeRate = expenseIncomeRate;
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public int getTotalIncome() {
+		return totalIncome;
+	}
+
+	public void setTotalIncome(int totalIncome) {
+		this.totalIncome = totalIncome;
+	}
+
+	public int getTotalLosses() {
+		return totalLosses;
+	}
+
+	public void setTotalLosses(int totalLosses) {
+		this.totalLosses = totalLosses;
+	}
+
+	public int getRemainMoney() {
+		return remainMoney;
+	}
+
+	public void setRemainMoney(int remainMoney) {
+		this.remainMoney = remainMoney;
+	}
+
+	public int getExpenseIncomeRate() {
+		return expenseIncomeRate;
+	}
+
+	public void setExpenseIncomeRate(int expenseIncomeRate) {
+		this.expenseIncomeRate = expenseIncomeRate;
+	}
+
+	@Override
+	public String toString() {
+		return "Analyze [userId=" + userId + ", totalIncome=" + totalIncome + ", totalLosses=" + totalLosses
+				+ ", remainMoney=" + remainMoney + ", expenseIncomeRate=" + expenseIncomeRate + "]";
+	}
+
 }
